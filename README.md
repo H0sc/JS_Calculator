4f7e2e79a4547745928c74ac7ca4a68bedb5454d
Timon Hosch <timon.hosch@gmail.com>
2023-09-07 15:23:56 +0200

    MVP finished

--- README.md
+++ README.md
@@ -1,43 +1,18 @@
-# JS_Calculator
+# notes for myself
 
-A calculator purely written with JavaScript
+## dataset attribut
 
-## Tools
+- dataset ist ein property der HTMLElement Schnittstelle. damit lassen sich daten, welche nicht sichtbar sein müssen, auf dem html element speichern.
 
-- Javascript
-- SASS for CSS
+## element.classList.add
 
-## Ziele
+- mit der add methode lassen sich über javascript temporär (?) klassen hinzufügen
 
-- Der Rechner soll in der Lage sein grundlegende arithmetische Rechnungen zu lösen (+,-,:,*), welche der Nutzer durch Klicken der Felder eingibt. Wie bei einem üblichen Taschenrechner soll der Nutzer in der Lage sein, die bisherigen Operationen und Ergebnisse gänzlich zu löschen oder nur die letzte getätigte Eingabe.
+## Array.from()
 
-- Weiterführendes Ziel ist das Hinzufügen komplexerer Rechenoperationen.
+- erstellt aus einem iterierbaren objekt mit längeneigenschaft oder jeglichem iterierbaren objekt ein array
 
-## Chronologisch geordnetes Vorgehen
+## generell
 
-### Setup / Grober Ablauf
-
-- Erstellen der HTML-Datei, der JS-, und CSS-Datei [X]
-- Installation und Einstellungen SASS [X]
-- Verknüpfung der Dateien []
-- Aufbau der HTML Struktur []
-- Aufbau Logik []
-- Styling []
-
-## Tasks im Detail
-
-### HTML
-
-#### Generell
-
-- Die Ganze App soll von einem Container-div umfasst sein.
-
-#### Display
-
-- Es gibt ein Display in Form eines div-Elements: Auf diesem Display wird der aktuelle Operand in einem span-Element angezeigt. Nach Anfügen eines Operators wird dieses span-Element um eine Zeile nach Oben verschoben und verkleinert, um das Input-div nicht vergrößern zu müssen. Der Input-div muss aber von Anfang an hoch genug sein, um dies zu gewährleisten.
-- Nach Klicken des "=" buttons, wird die Logik in der JS-Datei getriggered und das Ergebnis in das Display gefügt. Die vorangegangene Operation wird wieder nach Oben verschoben.
-
-#### Button-Logik
-
-- Zahlen: Zahl wird bei Klick an die vorige Zahl im Inputfeld angefügt. []
-- Zahlen: Kommastellen implementieren. parseFloat
+- event existiert nur innerhalb des eventlisteners.
+- frage dich, was, wann und wie passieren soll. sei exakt in deinen befehlen.
--- src/index.js
+++ src/index.js
@@ -1,51 +1,98 @@
-// const display = document.querySelectorAll(".display")[0]
-// display.children[0].value = ""
+const calculator = document.querySelector(".calculator")
+const keys = document.querySelector(".calculator__keys")
+const display = document.querySelector(".calculator__display")
 
-const input = document.querySelector(".display").children[0]
-input.style.textAlign = "end"
-const digitsChildren = document.querySelector(".digits").children
-
-document.body.onload = () => input.value = ""
-
-const handleDigits = (e) => {
-    console.log(e.target.value)
-    // zahl kommt rein als string und muss in float umgewandelt werden
-    // dafür muss die zahl geprüft werden auf die anzahl ihrer ziffern
-    // wenn die zahl länger ist, als drei ziffern, also bei 1000, wird nach der ersten ziffer ein punkt gesetzt
-    
-    // 1000 -> 1.000 -> 1 + 3
-    // 10000 -> 10.000
-    // 100.000
-    // 1.000.000 -> 1 + 6
-    // 10.000.000
-    // 100.000.000
-    // 1.000.000.000 -> 1 + 9 
-    // 10.000.000.000
-    // 100.000.000.000
-    // 1.000.000.000.000
-
-    // die länge der ziffern vor dem punkt ist immer maximal 3 
-    // die länge der zahl plus 3 ziffern, dann wird ein punkt gesetzt
-    // von hinten gezählt: der index des punktes plus 3, dann neuer punkt und neuer index ab dem gespeichert wird.  .+3, .+3, .+3, ...
-    // 999 ist die größte zahl 
-
-    // durchlaufe zahl(string).
-    // ist die zahl größer als 3 ziffern nacheinander? setze einen punkt nach dem ersten index
-    // jetzt ist eine zahl vor dem komma und 3 zahlen hinter dem komma. der punkt ist an 1. index.
-    // wenn zahl > 3, setze punkt ans ende. danach wiederholt sich folgendes pattern: 3 ziffern und ein punkt.
-    // die punkte verschieben sich nach rechts beim hinzufügen neuer ziffern. oder die zahlen verschieben sich nach links.
-    
-    // 
-    
+const setDisplayZero = () => {
+    display.textContent = "0"
 }
 
-const addEventsToDigits = () => {
-    for (let i = 0; i < digitsChildren.length; i++) {
-    console.log(`index ${i}`, digitsChildren[i].innerHTML)
-    digitsChildren[i].addEventListener("click", handleDigits)
-}
-}
-document.onload = addEventsToDigits()
+keys.addEventListener("click", (e) => {
+    let key = e.target
+    let action = key.dataset.action
+    let keyContent = key.textContent
+    let displayedNum = display.textContent
 
+    // wenn jeglicher button geklickt wird
+    if (e.target.matches("button")) {
+        Array.from(key.parentNode.children).forEach(el => {
+            el.classList.remove("is-depressed")
+        })
+    } else {
+        return console.log("you touched the grid! try to hit the buttons :V")
+    }
+    // buttons die keine action buttons sind; number keys
+    let previousKeyType = calculator.dataset.previousKeyType
+    if (!action) {
+        if (displayedNum === "0" || previousKeyType === "operator") {
+            display.textContent = keyContent
+            calculator.dataset.previousKeyType = "number"
+        } else if (previousKeyType === "number") {
+            display.textContent = displayedNum + keyContent
+        }
+    }
+
+    // wenn ein operator button geklickt wird
+    if (
+        action === "add" ||
+        action === "subtract" ||
+        action === "multiply" ||
+        action === "divide"
+    ) {
+        key.classList.add("is-depressed")
+        calculator.dataset.previousKeyType = "operator"
+        calculator.dataset.firstNum = displayedNum
+        calculator.dataset.operator = action
+    } 
+
+    if (action === "decimal") {
+        if (displayedNum.includes(".")) {
+            console.log("nur ein decimal pro number")
+        } else {
+            display.textContent = displayedNum + "."
+        }
+        // calculator.dataset.previousKeyType = "decimal"
+    }
+
+    const calculate = (n1, op, n2) => {
+        
+        let result = ""
+        if (op === "add") {
+            result = parseFloat(n1) + parseFloat(n2)
+        } else if (op === "subtract") {
+            result = parseFloat(n1) - parseFloat(n2)
+        } else if (op === "multiply") {
+            result = parseFloat(n1) * parseFloat(n2)
+        } else if (op === "divide") {
+            result = parseFloat(n1) / parseFloat(n2)
+        }
+        return result
+    }
+
+    if (action === "calculate" && calculator.dataset.previousKeyType != "decimal") {
+        let calcVals = {
+            firstNum: calculator.dataset.firstNum,
+            operator: calculator.dataset.operator,
+            secondNum: displayedNum
+        }
+
+        for (let prop in calcVals) {
+            if (prop === undefined) {
+                return
+            } else {
+                
+            }
+        }
+
+        console.log(calcVals.firstNum, calcVals.operator, calcVals.secondNum)
+
+        display.textContent = calculate(calcVals.firstNum, calcVals.operator, calcVals.secondNum)
+        // display.textContent = calculate(firstNum, operator, secondNum)
+    } 
+
+
+    if (action === "clear") {
+        display.textContent = "0"
+    }
+})
 
 
